ce90c492ff1f986f2f56465924821675bf6c3141
Josef Biehler <jbiehler@samhammer.de>
2023-12-04 17:46:08 +0100

    chapter functionenter init

---
+++ code/5-functionenter-x86/TestApp/TestApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TestApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.ReadKey();
+            Console.WriteLine("Hello, World!");
+            try
+            {
+                throw new Exception();
+            }
+            catch (Exception e)
+            {
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
