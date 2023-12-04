94551738926aa77e11ab442b1c53899f3f88bc20
Josef Biehler <jbiehler@samhammer.de>
2023-12-04 17:48:27 +0100

    init

---
+++ code/8-stackoverflow/ProfilerWorkshop/ProfilerWorkshop/Resource.h
@@ -0,0 +1,20 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Verwendet von ProfilerWorkshop.rc
+//
+
+#define IDS_PROJNAME                    100
+#define IDR_PROFILERWORKSHOP      101
+
+// Nächste Standardwerte für neue Objekte
+//
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        201
+#define _APS_NEXT_COMMAND_VALUE         32768
+#define _APS_NEXT_CONTROL_VALUE         201
+#define _APS_NEXT_SYMED_VALUE           106
+#endif
+#endif
+
+#define IDR_PROFILER 102
---
+++ code/8-stackoverflow/TestApp/TestApp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace TestApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.ReadKey();
+            Blub_i(1);
+            Blub_arr(new []{1,2,3,4,5,6});
+            Blub_str("Hello, World");
+            Console.ReadKey();
+        }
+
+        public static void Blub_i(int i)
+        {
+            Console.WriteLine(i);
+        }
+
+        public static void Blub_str(string str)
+        {
+            Console.WriteLine(str);
+        }
+
+        public static void Blub_arr(int[] intArray)
+        {
+            Console.WriteLine(intArray.Length);
+        }
+    }
+}
